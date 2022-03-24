f7894cfa0b87fd5b1dd7593a9c782087082f57ff
Andreas Reiff <Andreas@KHP-Services.de>
2022-03-24 12:31:28 +0100

    retries added on set date

--- KBVUpdateDownloader.ps1
+++ KBVUpdateDownloader.ps1
@@ -2,8 +2,9 @@
 # applying modify date as displayed on website
 
 # test $starturl = "https://update.kbv.de/ita-update/Abrechnung"
-$starturl = "https://update.kbv.de/ita-update" # no trailing /
+$starturl = "https://update.kbv.de/ita-update/" # with trailing /
 $targetroot = "C:\temp\kbv"
+$maxretries = 30
 
 function DownloadAll
 {
@@ -11,6 +12,9 @@ function DownloadAll
         $url,
         $target
     )
+
+    $url
+    $target
 
     $data = invoke-webrequest -uri $url
     $lines = ($data.Content -split "\r?\n|\r")
@@ -20,35 +24,68 @@ function DownloadAll
     {
         if ($line -match '<a href=".*">(?<subpath>.+)</a>.*(?<date>\d\d\d\d-\d\d-\d\d \d\d:\d\d)')
         {
-            $newurl = $url + "/" + $Matches.subpath
+            $newurl = $url + $Matches.subpath
             $date = $Matches.date
             $newpath = $target + "/" + $Matches.subpath
 
             # skip full iso + vorquartale
-            if ($newurl -like '*ITA-Update_202?_Q?.iso' -Or $newurl -like '*Vorquartale')
+            if ($newurl -like '*ITA-Update_202?_Q?.iso' -Or $newurl -like '*/Vorquartale/*')
             {
                  continue
             }
 
             # folder
             # debug output current data
-            $newurl
-            $newpath
+            #$newurl
+            #$newpath
             if ($newurl -like '*/')
             { 
                 If (!(test-path $newpath))
                 {
                     md $newpath
                 }
-                DownloadAll -url $newurl -target $newpath
-                $folder = Get-Item $newpath
-                $folder.LastWriteTime = [DateTime]$date
+                DownloadAll -url $newurl -target $newpath                
+                $tries = 0
+                while ($tries -lt $maxretries) {
+                    try{
+                        # retry, sometimes access denied exception, e. g. when going on network path
+                        $folder = Get-Item $newpath
+                        $folder.LastWriteTime = [DateTime]$date
+                        $tries = $maxretries;
+                    } catch {
+                        $tries = $tries + 1
+                        if ($tries -eq $maxretries)
+                        {
+                            #$tries
+                            #throw
+                            #$Error[0]
+                            #$_ | Out-String
+                            $_ | Write-Error
+                        }
+                    }
+                }
             } else
             # file
             {
                 Invoke-WebRequest $newurl -OutFile $newpath
-                $file = Get-Item $newpath
-                $file.LastWriteTime = [DateTime]$date
+                $tries = 0
+                while ($tries -lt $maxretries) {
+                    try{
+                        # retry, sometimes access denied exception, e. g. when going on network path
+                        $file = Get-Item $newpath
+                        $file.LastWriteTime = [DateTime]$date
+                        $tries = $maxretries;
+                    } catch {
+                        $tries = $tries + 1
+                        if ($tries -eq $maxretries)
+                        {
+                            #$tries
+                            #throw
+                            #$Error[0]
+                            $_ | Write-Error
+                        }
+                    }
+                }
             }
         }
     }
